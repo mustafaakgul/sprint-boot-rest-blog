48a379db76a3a12fa479818203da593da06f4bfa
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2024-01-30 23:16:35 +0300

    Upd user service

--- src/main/java/com/example/blog/service/UserService.java
+++ src/main/java/com/example/blog/service/UserService.java
@@ -1,15 +1,53 @@
 package com.example.blog.service;
 
-import blog.models.User;
+import com.example.blog.entity.User;
+import com.example.blog.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class UserService {
 
-    List<User> findAll();
-    User findById(Long id);
-    User create(User user);
-    User edit(User user);
-    void deleteById(Long id);
+    @Autowired
+    public UserRepository userRepository;
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> createUserList(List<User> list) {
+        return userRepository.saveAll(list);
+    }
+
+    public List<User> getUserList() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(int id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User updateUserById(User user) {
+        Optional<User> userFound = userRepository.findById(user.getId());
+
+        if (userFound.isPresent()) {
+            User userUpdate = userFound.get();
+            userUpdate.setFirstName(user.getFirstName());
+            userUpdate.setLastName(user.getLastName());
+            userUpdate.setAge(user.getAge());
+
+            return userRepository.save(user);
+        } else {
+            return null;
+        }
+    }
+
+    public String deleteUserById(int id) {
+        userRepository.deleteById(id);
+        return "User "+ id +" deleted";
+    }
 
 }
