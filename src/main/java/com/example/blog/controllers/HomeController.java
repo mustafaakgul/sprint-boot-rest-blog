679434e435459cde54606426d66d9d33a299cad9
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2024-01-30 22:45:23 +0300

    Upd unused import

--- src/main/java/com/example/blog/controllers/HomeController.java
+++ src/main/java/com/example/blog/controllers/HomeController.java
@@ -3,12 +3,9 @@ package com.example.blog.controllers;
 import com.example.blog.models.Post;
 import com.example.blog.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.stream.Collectors;
