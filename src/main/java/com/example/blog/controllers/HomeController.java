41c5b098ddc9af638dea3df0c066a993412ac61d
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2024-01-30 22:41:53 +0300

    Add index home

--- src/main/java/com/example/blog/controllers/HomeController.java
+++ src/main/java/com/example/blog/controllers/HomeController.java
@@ -3,18 +3,20 @@ package com.example.blog.controllers;
 import com.example.blog.models.Post;
 import com.example.blog.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
-import java.util.stream.Collectors;
-
-import static java.util.stream.Collectors.*;
+import java.util.stream.Collectors;;
 
 @Controller
 public class HomeController {
+
     @Autowired
     private PostService postService;
 
@@ -28,5 +30,16 @@ public class HomeController {
         model.addAttribute("latest2posts", latest2Posts);
 
         return "index";
+    }
+
+    @GetMapping(value = "")
+    public String index(@RequestParam(defaultValue = "1") int page, Model model) {
+        page = page < 1 ? 0 : page - 1;
+        Page<Post> posts = postService.getAllPublishedPostsByPage(page, appSetting.getPageSize());
+
+        model.addAttribute("totalPages", posts.getTotalPages());
+        model.addAttribute("posts", posts);
+        model.addAttribute("page", page + 1);
+        return "index/home";
     }
 }
