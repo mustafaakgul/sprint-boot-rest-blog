9cf4fe6513456f18b82f5bc72647ec7920b808e3
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2024-01-30 23:55:46 +0300

    Add health check

---
+++ src/main/java/org/project/springbootblog/controller/HealthCheckController.java
@@ -0,0 +1,15 @@
+package org.project.springbootblog.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/healthcheck")
+public class HealthCheckController {
+
+    @RequestMapping("/")
+    public ResponseEntity<String> index() {
+        return ResponseEntity.ok("OK");
+    }
+}
