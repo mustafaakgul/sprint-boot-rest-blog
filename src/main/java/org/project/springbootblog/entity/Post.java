265300db860dc196c156d06efbaef01c374df2dc
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2024-01-31 00:57:02 +0300

    Add post entity

---
+++ src/main/java/org/project/springbootblog/entity/Post.java
@@ -0,0 +1,83 @@
+package org.project.springbootblog.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = true)
+    private String content;
+
+    /*@ManyToOne(optional = false, fetch = FetchType.LAZY)
+    private User author;*/
+
+    @Column(nullable = false)
+    private Date date = new Date();
+
+    public Post() {
+    }
+
+    public Post(long id, String title, String content, Date date) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.date = date;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", date=" + date +
+                '}';
+    }
+}
