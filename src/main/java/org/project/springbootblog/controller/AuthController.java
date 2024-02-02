00c4c4cf0d3a21e871ffa78d91c481d5520c555b
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2024-02-02 22:32:19 +0300

    Add auth controller

---
+++ src/main/java/org/project/springbootblog/controller/AuthController.java
@@ -0,0 +1,53 @@
+package org.project.springbootblog.controller;
+
+import org.project.springbootblog.entity.User;
+import org.project.springbootblog.model.request.LoginRequest;
+import org.project.springbootblog.model.response.ErrorResponse;
+import org.project.springbootblog.model.response.LoginResponse;
+import org.project.springbootblog.security.auth.JwtUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/rest/auth")
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+
+
+    private JwtUtil jwtUtil;
+    public AuthController(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    public ResponseEntity login(@RequestBody LoginRequest loginReq)  {
+
+        try {
+            Authentication authentication =
+                    authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginReq.getEmail(), loginReq.getPassword()));
+            String email = authentication.getName();
+            User user = new User(email,"");
+            String token = jwtUtil.createToken(user);
+            LoginResponse loginRes = new LoginResponse(email,token);
+
+            return ResponseEntity.ok(loginRes);
+
+        }catch (BadCredentialsException e){
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST,"Invalid username or password");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }catch (Exception e){
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }
+    }
+}
