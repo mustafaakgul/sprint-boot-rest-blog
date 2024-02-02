6fc181bbc3dee4e5e837f3c523c024ade98960c6
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2024-02-03 00:47:53 +0300

    Upd user serv

--- src/main/java/org/project/springbootblog/service/CustomUserDetailsService.java
+++ src/main/java/org/project/springbootblog/service/CustomUserDetailsService.java
@@ -6,20 +6,15 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
-
     private final UserRepository userRepository;
-
     public CustomUserDetailsService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userRepository.findUserByEmail(email);
